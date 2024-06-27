1ce52e271416f161f2b5a73ed07fe10affd3045e
RosyDiVilio <165782561+RosyDiVilio@users.noreply.github.com>
2024-06-27 16:54:43 +0200

    add: abstract animals modificato

--- src/main/java/org/lessons/java/abstractanimals/main.java
+++ src/main/java/org/lessons/java/abstractanimals/main.java
@@ -7,25 +7,29 @@ public class main {
 		Cane rex = new Cane();
 		System.out.println("Cane =");
 		System.out.println("Mangia: " + rex.mangia());
-		System.out.println("Verso: " + rex.dormi());
+		System.out.println("Verso: " + rex.verso());
+		System.out.println("Dorme: " + rex.dormi());
 		System.out.println("------------");
 		
 		Passerotto titti = new Passerotto();
 		System.out.println("Passerotto =");
 		System.out.println("Mangia: " + titti.mangia());
-		System.out.println("Verso: " + titti.dormi());
+		System.out.println("Verso: " + titti.verso());
+		System.out.println("Dorme: " + titti.dormi());
 		System.out.println("------------");
 		
 		Aquila america = new Aquila();
 		System.out.println("Aquila =");
 		System.out.println("Mangia: " + america.mangia());
-		System.out.println("Verso: " + america.dormi());
+		System.out.println("Verso: " + america.verso());
+		System.out.println("Dorme: " + america.dormi());
 		System.out.println("------------");
 		
 		Delfino flipper = new Delfino();
 		System.out.println("Delfino =");
 		System.out.println("Mangia: " + flipper.mangia());
-		System.out.println("Verso: " + flipper.dormi());
+		System.out.println("Verso: " + flipper.verso());
+		System.out.println("Dorme: " + flipper.dormi());
 		System.out.println("------------");
 	
 	}
